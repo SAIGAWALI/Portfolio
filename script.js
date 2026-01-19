be5c3a8aaa2a76b6a9b7cf512c6b63cfc835c3aa
SAIGAWALI <23110114@iitgn.ac.in>
2026-01-20 01:12:15 +0530

    details updated

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Scroll smoothly when clicking anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      target.scrollIntoView({ behavior: "smooth" });
-    });
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
-  function openLink(url) {
-    // Open the provided URL in a new tab
-    window.open(url, "_blank");
-  }
-  
+});
+
+function openLink(url) {
+  window.open(url, "_blank");
+}
